4ed5d2cd27f1b015873765fa06dced01a2ca5c05
Lucas Serafim <dev.lucas.serafin@protonmail.com>
2021-08-24 23:07:40 -0300

    [letmeask-aula-01] bug do env resolvido

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 export function Button() {
   const [counter, setCounter] = useState(0)
 
-  function increment(){
+  function increment() {
     setCounter(counter + 1);
   }
 
-  return(
-    <button onClick={increment}>{counter}</button>
+  return (
+    <button onClick={increment}>
+      {counter}
+    </button>
   )
 }
