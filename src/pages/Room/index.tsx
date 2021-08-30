b766d247f00dc2deba1983792a75444a541ca120
Lucas Serafim <lucas.serafim@fornecedoramaquinas.com.br>
2021-08-30 16:54:43 -0300

    [Letmeask Aula-04] Parte da aula 04 / Auteraçao nas pastas e arquivos

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -2,14 +2,15 @@ import { useEffect } from 'react';
 import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import logoImg from '../assets/images/logo.svg';
+import logoImg from '../../assets/images/logo.svg';
 
-import { Button } from '../components/Button';
-import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
-import { database } from '../services/firebase';
+import { Button } from '../../components/Button';
+import { Question } from '../../components/Question';
+import { RoomCode } from '../../components/RoomCode';
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
 
-import '../styles/room.scss';
+import './styles.scss';
 
 type FirebaseQuestions = Record<string, {
   author: {
@@ -21,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isHighLighted: boolean;
 }>;
 
-type  Question = {
+type  QuestionType = {
   id: string;
   author: {
     name: string;
@@ -40,7 +41,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
   const roomId = params.id;
@@ -132,6 +133,20 @@ export function Room() {
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
+
+        <div className="question-list">
+          {questions.map((question) => {
+              return (
+                <Question 
+                  key={question.id}
+                  content={question.content}
+                  author={question.author}
+                  
+                />
+              );
+            })}
+        </div>
+
       </main>
     </div>
   )
